463c4639bdd0e6dcec19598724da7b9db72a366b
paleologos <paleologia@gmail.com>
2022-07-15 22:28:10 +0200

    add installer.sh

---
+++ installer.sh
@@ -0,0 +1,16 @@
+
+echo "========================================================="
+echo "Updating system"
+echo "========================================================="
+read -p "Press enter to continue / Pritisnite Enter da bi ste nastavili:  "
+sleep 1 
+sudo apt update -y
+
+echo "========================================================="
+echo "Installing Nodejs and NPM "
+echo "========================================================="
+read -p "Press enter to continue / Pritisnite Enter da bi ste nastavili:  "
+sleep 1 
+sudo apt install curl -y
+sleep 1 
+read -p "Press enter to continue / Pritisnite Enter da bi ste nastavili:  "
